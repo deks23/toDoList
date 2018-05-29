57123d83a461bce374193c4dfb6d5b1a07a62f30
Damian <damiankotynia@MacBook-Air-Damian.local>
2018-05-29 20:55:57 +0200

    litle changes

--- src/main/resources/static/js/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    console.log("HELLO");
-    $.ajax({
-        url: "localhost:8080/rest/GetTasks"
-    }).then(function(data) {
-       console.log(data);
-    });
-});
